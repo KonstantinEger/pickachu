3de603898ac65bf4e4d77f93f5c1194b76d2ee26
ke51wade <ke51wade@ws-i-rob02.gr>
2023-04-13 14:06:05 +0200

    init

---
+++ pickachu-bot/src/pickachu/bot/PickachuBot.java
@@ -0,0 +1,10 @@
+package pickachu.bot;
+
+import lejos.utility.Delay;
+
+public class PickachuBot {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		Delay.msDelay(5000);
+	}
+}

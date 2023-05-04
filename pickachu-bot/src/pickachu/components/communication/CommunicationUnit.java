dc0970b4152c22f8a58b1456b349eaa563a1c5de
Zylence <kirsch.julian@yahoo.com>
2023-05-04 15:07:16 +0200

    Swapped out manual TCP in favour of Websocket Library, further added more OpCodes

--- pickachu-bot/src/pickachu/components/communication/CommunicationUnit.java
+++ pickachu-bot/src/pickachu/components/communication/CommunicationUnit.java
@@ -1,56 +1,23 @@
 package pickachu.components.communication;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
+import java.net.InetSocketAddress;
 
-import pickachu.components.ComponentInitializationError;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
 
 /**
  * Contains all the logic necessary to send/receive tcp messages that arrive via the enclosed Socket Object
  * It offers a "comfortable" abstracted way of sending/receiving messages.
  */
-public class CommunicationUnit {
+public class CommunicationUnit extends WebSocketServer {
 	
-	public static String address = "";
-	public static int port = 123;
-	
-	
-	private final Socket socket;
+	public static InetSocketAddress address = new InetSocketAddress(8081);
 	private MessageHandler handler;
-	private final Receiver receiver;
-	BufferedWriter sender;
 	
 	public CommunicationUnit() {
-		try {
-			this.socket = new Socket(address, port);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new ComponentInitializationError("Socket connection could not be established");
-		}
-		
-		try {
-			this.receiver = new Receiver();
-			this.receiver.start();
-		} catch (IOException e) {
-			throw new ComponentInitializationError("Could not retrieve InputStream from socket.");
-		}
-		
-		OutputStream outputStream;
-		try {
-			outputStream = socket.getOutputStream();
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-			sender = new BufferedWriter(outputStreamWriter);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new ComponentInitializationError("Could not retrieve OutputStream from socket.");
-
-		}
+		super(address);
 	}
 	
 	
@@ -61,49 +28,40 @@ public class CommunicationUnit {
 		this.handler = handler;
 	}
 	
-	/**
-	 * Sends a message to the server.
-	 * @return true if a message was sent successfully
-	 */
-	public boolean sendMessage(Message message) {
-		try {
-			sender.write(message.parseMessageToString()); //todo does writer write \n at the end of the msg? (we need that)
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	
 
-	/**
-	 * Responsible for waiting on incoming messages, parsing them into a Message Object and triggering the handle function
-	 * of our {@link MessageHandler}. This listens until the underlying InputStream is stopped.
-	 */
-	class Receiver extends Thread{
+	@Override
+	public void onClose(WebSocket arg0, int arg1, String arg2, boolean arg3) {
+		// TODO Auto-generated method stub
 		
-		private BufferedReader reader;
+	}
+
+
+	@Override
+	public void onError(WebSocket arg0, Exception arg1) {
+		// TODO Auto-generated method stub
 		
-		public Receiver() throws IOException {
-			InputStream input = socket.getInputStream();
-			InputStreamReader streamReader = new InputStreamReader(input);
-			reader = new BufferedReader(streamReader);
-		}
+	}
+
+
+	@Override
+	public void onMessage(WebSocket sender, String message) {
+		 Message response = handler.handle(Message.parseStringToMessage(message));
+		 if (response != null && sender.isOpen()) {
+			 sender.send(response.parseMessageToString());
+		 }
+	}
+
+
+	@Override
+	public void onOpen(WebSocket arg0, ClientHandshake arg1) {
+		// TODO Auto-generated method stub
 		
-		@Override
-		public void run() {
-			String clientInput = "";
-			while (clientInput != null) {	
-				try {
-					clientInput = reader.readLine();
-					Message response = handler.handle(Message.parseStringToMessage(clientInput));
-					if (response != null) {
-						sendMessage(response);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} 
-			}	
-		}
+	}
+
+
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- pickachu-bot/src/pickachu/components/communication/OpCode.java
+++ pickachu-bot/src/pickachu/components/communication/OpCode.java
@@ -7,7 +7,12 @@ import java.util.Map;
  * OpCode written at the start of every message to make it identifable
  */
 public enum OpCode {
-	NoOp;
+	NoOp, //not viable
+	Stop,
+	Drive, // [(x,y), (x1,y1)]
+	PickUp, // tells the robot to pick the object up
+	Drop, // tells the robot to drop the object
+	;
 	
 	public static Map <String, OpCode> stringLookup = new HashMap<>();
 	
